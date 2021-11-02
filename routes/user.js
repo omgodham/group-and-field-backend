d75d823ecdf5d3ce3f758a10e7474d5f80b26c17
omkargodham <omkargodham321@gmail.com>
2021-11-02 17:29:44 +0530

    routes for particular childs and students for respective parent and teacher

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { verifyTeacher ,isSignedIn ,verifyTeacherOrAdmin} = require('../controllers/auth');
 const router = express.Router();
-const {getUserById,getUser,updateUser,deleteUser,getAllUsersWithSameStd,updateLectures,getAllStudents,getAllTeachers} = require('../controllers/user');
+const {getUserById,getUser,updateUser,deleteUser,getAvailableChilds,getAllUsersWithSameStd,getAllStudentsUnderTeacher,updateLectures,getAllStudents,getAllTeachers} = require('../controllers/user');
 
 router.param('userId',getUserById);
 
@@ -14,6 +14,7 @@ router.get('/students/all-students'  , getAllStudents)
 router.get('/users/:userId/:usersStd',verifyTeacherOrAdmin,getAllUsersWithSameStd)
 router.get('/teachers/all-teachers' ,getAllTeachers)
 router.put('/lectures/update-lectures/:userId',updateLectures)
-
+router.get('/students/teacher/:userId/:studentId',verifyTeacherOrAdmin,getAllStudentsUnderTeacher)
+router.get('/childs/available-childs',verifyTeacherOrAdmin,getAvailableChilds)
 
 module.exports = router;
